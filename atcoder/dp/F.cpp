8817326e27af91a44096c7ab6edefe0d8a9e75a3
Mayank Bist <mayankbist45@gmail.com>
2019-04-28 16:16:00 +0530

    Add solution for problem `F - LCS` Link: https://atcoder.jp/contests/dp/tasks/dp_f Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/5175735

--- atcoder/dp/F.cpp
+++ atcoder/dp/F.cpp
@@ -1,17 +1,20 @@
-#include <bits/stdc++.h>
+#include "bits/stdc++.h"
 
-#define int long long
 using namespace std;
 
-int32_t main() {
+using ll = long long;
+
+int main() {
 
     string s, t;
     cin >> s >> t;
 
-    int a = s.size(), b = t.size();
+    int a = s.size();
+    int b = t.size();
+
     int dp[a + 1][b + 1] = {};
 
-    for (int i = 1; i <= a; i++) {
+    for (int i = 1; i <= a; ++i) {
         for (int j = 1; j <= b; ++j) {
             if (s[i - 1] == t[j - 1])
                 dp[i][j] = 1 + dp[i - 1][j - 1];
@@ -19,22 +22,25 @@ int32_t main() {
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
         }
     }
+//    cout << dp[a][b];
+
+    int i = a , j = b;
     string ans;
-    int i = a, j = b;
-    while (i > 0 and j > 0) {
-        if (s[i - 1] == t[j - 1]) {
-            ans += s[i - 1];
+    while(i>0 && j > 0){
+        if(s[i-1] == t[j-1]){
+            ans += s[i-1];
             i--;
             j--;
-        } else {
-            if (dp[i - 1][j] > dp[i][j - 1])
+        }
+        else
+        {
+            if(dp[i-1][j] > dp[i][j-1])
                 i--;
             else
                 j--;
         }
     }
-    reverse(ans.begin() ,ans.end());
-
-    cout << ans << endl;
+    reverse(ans.begin(),ans.end());
+    cout<<ans;
 }
 
