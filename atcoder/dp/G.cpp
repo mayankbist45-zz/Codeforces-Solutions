6f9aaf6c11a954985267a110d94cb5be4be9dcee
Mayank Bist <mayankbist45@gmail.com>
2019-04-28 17:30:00 +0530

    Add solution for problem `G - Longest Path` Link: https://atcoder.jp/contests/dp/tasks/dp_g Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/5176538

--- atcoder/dp/G.cpp
+++ atcoder/dp/G.cpp
@@ -1,42 +1,39 @@
 #include <bits/stdc++.h>
-
-#define int long long
 using namespace std;
-#define maxn 100010
-int n,m;
-vector<int> g[maxn];
-int indegree[maxn] ,dist[maxn] ;
-bool visited[maxn];
 
-void dfs(int v,int p){
-    visited[v] = true;
-    for(auto x: g[v]){
-        if(x == p)continue;
-        dist[x] = max(dist[x] , 1 + dist[v]);
-        indegree[x]--;
-        if(indegree[x] == 0 and !visited[x])
-            dfs(x ,v);
+const int maxn = 1e5 + 5;
+
+bool visited[maxn];
+int dist[maxn];
+vector<int> g[maxn];
+int indegree[maxn];
+
+void dfs(int a){
+    visited[a] = true;
+    for(auto b : g[a]){
+        dist[b] = max(dist[b] , 1 + dist[a]);
+        --indegree[b];
+        if(indegree[b] == 0)
+            dfs(b);
     }
 }
 
-int32_t main() {
+int main(){
+    int n,m;
+    cin>>n>>m;
 
-    cin >> n >> m;
+    for(int u=0;u<m;u++){
+        int a,b;
+        cin>>a>>b;
 
-    int a ,b;
-    for (int i = 0; i < m; ++i) {
-        cin >> a >> b;
         g[a].push_back(b);
         indegree[b]++;
     }
-    for(int i = 1; i <= n ;i++){
-        if(!visited[i] and indegree[i] == 0)
-            dfs(i , -1);
-    }
-    int ans = 0;
-    for (int j = 1; j <= n; ++j) {
-        ans = max(ans,dist[j]);
-    }
-    cout << ans;
-}
 
+    for(int i=1;i<=n;i++){
+        if(!visited[i] && indegree[i] == 0)
+            dfs(i);
+    }
+
+    cout<<*max_element(dist,dist+maxn);
+}
