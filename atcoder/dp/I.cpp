e33a8ab4f3d875d04463498197224d0dbc418c48
Mayank Bist <mayankbist45@gmail.com>
2019-05-29 16:30:00 +0530

    Add solution for problem `I - Coins` Link: https://atcoder.jp/contests/dp/tasks/dp_i Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/5692234

--- atcoder/dp/I.cpp
+++ atcoder/dp/I.cpp
@@ -2,37 +2,29 @@
 
 using namespace std;
 
-#define int long long
-#define endl '\n'
-#define maxn 100010
-const int MOD = 1000000007;
+double ar[3000];
+int n;
+double dp[3000][3000];
+bool visited[3000][3000];
 
-void solve() {
-    int n;
-    cin >> n;
-    vector<double> p(n);
-    vector<vector<double>> dp(n + 1, vector<double>(n + 1));
-    for (int i = 0; i < n; i++)cin >> p[i];
-    dp[0][0] = 1;
-    for (int i = 1; i <= n; i++)
-        for (int j = 0; j <= i; j++) {
-            if (j)dp[i][j] = dp[i - 1][j - 1] * p[i - 1];
-            dp[i][j] += dp[i - 1][j] * (1 - p[i - 1]);
-        }
-    double ans = 0;
-    for (int i = 0; i <= n; i++)
-        if (i > n - i)ans += dp[n][i];
-    cout << fixed << setprecision(12) << ans << endl;
+double solve(int i, int heads) {
+    if (i == n)
+        return heads * 2 >= n;
+    double &ans = dp[i][heads];
+    if (visited[i][heads])
+        return ans;
+    visited[i][heads] = true;
+    ans = ar[i] * solve(i + 1, heads + 1) + (1-ar[i]) * solve(i+1 , heads);
+    return ans;
 }
 
-int32_t main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main() {
 
-    int t = 1;
-//    cin >> t;
+    cin >> n;
 
-    while (t--)
-        solve();
+    for (int i = 0; i < n; i++)
+        cin >> ar[i];
+    cout << setprecision(10) << fixed << solve(0, 0) << endl;
+
     return 0;
 }
