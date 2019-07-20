93f1cea9710b3cbc4bad7a564eb8a49d287f25a2
Mayank Bist <mayankbist45@gmail.com>
2019-07-20 17:31:00 +0530

    Add solution for problem `A - Dodecagon` Link: https://atcoder.jp/contests/abc134/tasks/abc134_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc134/submissions/6447098

---
+++ atcoder/abc134/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define MOD 1000000007
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    cout << 3 * n * n ;
+
+    return 0;
+}
