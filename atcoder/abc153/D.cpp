1e3ee215412a397ba657412e142e9a89abfc6d0e
Mayank Bist <mayankbist45@gmail.com>
2020-01-27 14:48:00 +0530

    Add solution for problem `D - Caracal vs Monster` Link: https://atcoder.jp/contests/abc153/tasks/abc153_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc153/submissions/9782625

---
+++ atcoder/abc153/D.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define MOD 1000000007
+#define maxn 100010
+
+void solve() {
+    int h;
+    cin >> h;
+
+    int ans = 0, p = 1;
+
+    while (h > 0) {
+        ans += p;
+        h /= 2;
+        p *= 2;
+    }
+    cout << ans << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
