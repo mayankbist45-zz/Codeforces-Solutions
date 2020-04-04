18743ab907cb0031835fc90f54fe19c6c100ac13
Mayank Bist <mayankbist45@gmail.com>
2020-04-04 17:34:00 +0530

    Add solution for problem `A - ABC Swap` Link: https://atcoder.jp/contests/abc161/tasks/abc161_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc161/submissions/11496696

---
+++ atcoder/abc161/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << c << " " << a << " " << b;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
