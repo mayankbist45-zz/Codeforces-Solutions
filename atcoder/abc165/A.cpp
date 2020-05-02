301032acdd2582757c90e8ffa341d15115701000
Mayank Bist <mayankbist45@gmail.com>
2020-05-02 17:41:00 +0530

    Add solution for problem `A - We Love Golf` Link: https://atcoder.jp/contests/abc165/tasks/abc165_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc165/submissions/12579353

---
+++ atcoder/abc165/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int k, a, b;
+    cin >> k >> a >> b;
+
+    if(b - b % k >= a)
+        cout << "OK";
+    else cout << "NG";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
