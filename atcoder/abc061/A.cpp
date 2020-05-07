47a91446dba4b1e42bc532f8b3503778087f0e99
Mayank Bist <mayankbist45@gmail.com>
2020-05-07 23:35:00 +0530

    Add solution for problem `A - Between Two Integers` Link: https://atcoder.jp/contests/abc061/tasks/abc061_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc061/submissions/12937016

---
+++ atcoder/abc061/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+using int32 = __uint32_t;
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(c >= a and c <= b)cout << "Yes";
+    else cout << "No";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
