c26652608a18aec028942b1209c59ccf882085b1
Mayank Bist <mayankbist45@gmail.com>
2020-05-09 18:47:00 +0530

    Add solution for problem `A - Sheep and Wolves` Link: https://atcoder.jp/contests/abc164/tasks/abc164_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc164/submissions/12989991

---
+++ atcoder/abc164/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+using int32 = __uint32_t;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    if(b >= a)cout<<"unsafe";
+    else cout << "safe";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
