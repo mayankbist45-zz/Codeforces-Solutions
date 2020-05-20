a01dc49eeec87563bfcd74c9e6e0433f192b4d8d
Mayank Bist <mayankbist45@gmail.com>
2020-05-20 11:44:00 +0530

    Add solution for problem `C - Replacing Integer` Link: https://atcoder.jp/contests/abc161/tasks/abc161_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc161/submissions/13422393

---
+++ atcoder/abc161/C.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    n %= k;
+    cout << min(n, abs(k - n));
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
