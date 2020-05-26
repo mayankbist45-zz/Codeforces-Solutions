d4514fedcffaea8d6518eb09f397f75cb6e20fc5
Mayank Bist <mayankbist45@gmail.com>
2020-05-26 11:03:00 +0530

    Add solution for problem `E - 1 or 2` Link: https://atcoder.jp/contests/abc126/tasks/abc126_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc126/submissions/13612354

---
+++ atcoder/abc126/E.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+int visited[maxn];
+vector<int> g[maxn];
+
+void dfs(int u) {
+    visited[u] = true;
+    for (auto x : g[u]) {
+        if (!visited[x]) {
+            dfs(x);
+        }
+    }
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int a, b, z;
+        cin >> a >> b >> z;
+
+        g[a].push_back(b);
+        g[b].push_back(a);
+    }
+    int ct = 0;
+    for (int i = 1; i <= n; i++)
+        if (!visited[i])
+            dfs(i), ct++;
+        cout << ct << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
