273d3f30f04975f88996d6157cc6a49859138e5b
Mayank Bist <mayankbist45@gmail.com>
2020-07-24 17:34:00 +0530

    Add solution for problem `D2 - Too Many Segments (hard version)` Link: https://codeforces.com/contest/1249/problem/D2 Tags: data structures, greedy, sortings, *1800 Ref: https://codeforces.com/contest/1249/submission/87858545

--- codeforces/1249/D2.cpp
+++ codeforces/1249/D2.cpp
@@ -10,18 +10,53 @@ const int MOD = 1000000007;
 vector<pair<int, int>> v[maxn];
 int n, k;
 
-int bit[maxn];
+int lazy[4 * maxn], tree[4 * maxn];
 
-void add(int idx, int val) {
-    for (; idx <= maxn - 10; idx += idx & (-idx))
-        bit[idx] += val;
+int doit(int a, int b) {
+    return max(a, b);
 }
 
-int pref(int idx) {
-    int ans = 0;
-    for (; idx > 0; idx -= idx & (-idx))
-        ans += bit[idx];
-    return ans;
+void divide(int index, int l, int r) {
+    tree[index] += lazy[index];
+    if (l != r) {
+        lazy[2 * index] += lazy[index];
+        lazy[2 * index + 1] += lazy[index];
+    }
+    lazy[index] = 0;
+}
+
+int queryTree(int start, int end, int l, int r, int index = 1) {
+    if (lazy[index] != 0) {
+        divide(index, start, end);
+    }
+    if (r < start or l > end)
+        return LLONG_MIN;
+
+    if (l <= start and r >= end)
+        return tree[index];
+
+    int mid = start + end >> 1;
+    return doit(queryTree(start, mid, l, r, 2 * index), queryTree(mid + 1, end, l, r, 2 * index + 1));
+}
+
+void update(int start, int end, int l, int r, int value, int index) {
+    if (lazy[index] != 0) {
+        divide(index, start, end);
+    }
+
+    if (r < start or l > end)
+        return;
+
+    if (l <= start and r >= end) {
+        lazy[index] += value;
+        divide(index, start, end);
+        return;
+    }
+
+    int mid = start + end >> 1;
+    update(start, mid, l, r, value, 2 * index);
+    update(mid + 1, end, l, r, value, 2 * index + 1);
+    tree[index] = doit(tree[2 * index], tree[2 * index + 1]);
 }
 
 void solve() {
@@ -30,8 +65,7 @@ void solve() {
     for (int i = 0; i < n; i++) {
         int l, r;
         cin >> l >> r;
-        add(l,1);
-        add(r + 1, -1);
+        update(1, 200000, l, r, 1, 1);
         v[l].push_back({r, i});
     }
 
@@ -39,14 +73,13 @@ void solve() {
     priority_queue<pair<pair<int, int>, int>> pq;
     for (int i = 0; i <= 200000; i++) {
         for (auto x: v[i])pq.push({{x.first, i}, x.second});
-        int qu = pref(i);
+        int qu = queryTree(1, 200000, i, i, 1);
         while (qu > k) {
             auto top = pq.top();
             pq.pop();
 
             ans[top.second] = 1;
-            add(top.first.second, -1);
-            add(top.first.first + 1, 1);
+            update(1, 200000, top.first.second, top.first.first, -1, 1);
             qu--;
         }
     }
