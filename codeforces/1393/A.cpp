a8a2380fb2fa734541ca92a3467164e17d66f52b
Mayank Bist <mayankbist45@gmail.com>
2020-08-07 20:12:00 +0530

    Add solution for problem `A - Rainbow Dash, Fluttershy and Chess Coloring` Link: https://codeforces.com/contest/1393/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1393/submission/89215198

---
+++ codeforces/1393/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    if(n & 1)cout << (n + 1)/2 << endl;
+    else cout << n/2 + 1 << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
