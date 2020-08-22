8fd4437b93f73310d4f72c68e98a1635bd4adf6b
Mayank Bist <mayankbist45@gmail.com>
2020-08-22 17:37:00 +0530

    Add solution for problem `B - Multiple of 9` Link: https://atcoder.jp/contests/abc176/tasks/abc176_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc176/submissions/16110080

---
+++ atcoder/abc176/B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    int sum = 0;
+
+    for (auto x: s)sum += (x - '0');
+    if (sum % 9 == 0)cout << "Yes";
+    else cout << "No";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
