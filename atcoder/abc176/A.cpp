888ebf10e28e41208f7a6138733fef9e6345af55
Mayank Bist <mayankbist45@gmail.com>
2020-08-22 17:32:00 +0530

    Add solution for problem `A - Takoyaki` Link: https://atcoder.jp/contests/abc176/tasks/abc176_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc176/submissions/16098848

---
+++ atcoder/abc176/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int n, x, t;
+    cin >> n >> x >> t;
+
+    int tp = n/x + (n % x > 0);
+    cout << tp * t << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
