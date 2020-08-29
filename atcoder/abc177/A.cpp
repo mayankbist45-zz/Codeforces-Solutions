05b264f8c298cb6e134d088ce8d28fb66f561826
Mayank Bist <mayankbist45@gmail.com>
2020-08-29 17:32:00 +0530

    Add solution for problem `A - Don't be late` Link: https://atcoder.jp/contests/abc177/tasks/abc177_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc177/submissions/16309674

---
+++ atcoder/abc177/A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int d, t, s;
+    cin >> d >> t >> s;
+
+    double p =(double) d / (double)t;
+    if (s >= p)cout << "Yes";
+    else cout << "No";
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
