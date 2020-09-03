edd4ccfcb7568d4ed7519d585af5cb3ca7ff59e6
Mayank Bist <mayankbist45@gmail.com>
2020-09-04 01:49:00 +0530

    Add solution for problem `D - Count Good Substrings` Link: https://codeforces.com/contest/451/problem/D Tags: math, *2000 Ref: https://codeforces.com/contest/451/submission/91750570

---
+++ codeforces/451/D.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    int even = 0, odd = 0;
+    map<int, int> ev, od;
+    for (int i = 0; i < s.size(); i++) {
+        odd++;
+        if (i & 1) {
+            even += ev[s[i]];
+            odd += od[s[i]];
+        } else {
+            odd += ev[s[i]];
+            even += od[s[i]];
+        }
+
+        if (i & 1)od[s[i]]++;
+        else ev[s[i]]++;
+    }
+    cout << even - 1 + 1 << " " << odd << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
