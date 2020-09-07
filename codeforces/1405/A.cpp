10f44de3aea75f723b35a9b86d20895d712c6f8e
Mayank Bist <mayankbist45@gmail.com>
2020-09-07 12:06:00 +0530

    Add solution for problem `A - Permutation Forgery` Link: https://codeforces.com/contest/1405/problem/A Tags: constructive algorithms, *800 Ref: https://codeforces.com/contest/1405/submission/92110625

---
+++ codeforces/1405/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    reverse(v.begin(), v.end());
+    for (auto x: v)cout << x << " ";
+    cout << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
