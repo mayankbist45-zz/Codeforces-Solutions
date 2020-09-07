87e57a6a6b0b90db4afa789b7c637dc2254c4950
Mayank Bist <mayankbist45@gmail.com>
2020-09-07 17:39:00 +0530

    Add solution for problem `F - Subsequences of Length Two` Link: https://codeforces.com/contest/1409/problem/F Tags: dp, strings, *2100 Ref: https://codeforces.com/contest/1409/submission/92131791

--- codeforces/1409/F.cpp
+++ codeforces/1409/F.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define maxn 210
 const int MOD = 1000000007;
 
-int n, kk;
+int n, k;
 string a, b;
 
 /*
@@ -16,30 +16,24 @@ string a, b;
 
 int dp[maxn][maxn][maxn];
 
-void max_self(int &a, int b) {
-    a = max(a, b);
+int solve(int p1, int p2, int step) {
+    if (step > k)return INT_MIN;
+    if (p1 == a.size())return 0;
+    int &ans = dp[p1][p2][step];
+    if (ans != -1)return ans;
+    ans = 0;
+    ans = max(ans, solve(p1 + 1, p2 + (a[p1] == b[0]), step) + (a[p1] == b[1] ? p2 : 0));
+    //change to b0
+    ans = max(ans, solve(p1 + 1, p2 + 1, step + 1) + (b[0] == b[1] ? p2 : 0));
+    // change to b1
+    ans = max(ans, solve(p1 + 1, p2 + (b[0] == b[1]), step + 1) + p2);
+    return ans;
 }
 
 void solve() {
     memset(dp, -1, sizeof dp);
-    cin >> n >> kk >> a >> b;
-
-    dp[0][0][0] = 0;
-    for (int i = 0; i < a.size(); i++) {
-        for (int k = 0; k <= kk; k++) {
-            for (int j = 0; j <= n; j++) {
-                if (dp[i][j][k] == -1)continue;
-                max_self(dp[i + 1][j + (a[i] == b[0])][k], dp[i][j][k] + (a[i] == b[1] ? j : 0));
-                //change to b0
-                max_self(dp[i + 1][j + 1][k + 1], dp[i][j][k] + (b[0] == b[1] ? j : 0));
-                //change to b1
-                max_self(dp[i + 1][j + (b[0] == b[1])][k + 1], dp[i][j][k] + j);
-            }
-        }
-    }
-    int ans = 0;
-    for (int j = 0; j <= n; j++)for (int steps = 0; steps <= kk; steps++)ans = max(ans, dp[a.size()][j][steps]);
-    cout << ans << endl;
+    cin >> n >> k >> a >> b;
+    cout << solve(0, 0, 0) << endl;
 }
 
 int32_t main() {
