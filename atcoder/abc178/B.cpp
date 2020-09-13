f553d4eb875458ce8e32f5ef2a5cfd3a1cfa8d4a
Mayank Bist <mayankbist45@gmail.com>
2020-09-13 17:33:00 +0530

    Add solution for problem `B - Product Max` Link: https://atcoder.jp/contests/abc178/tasks/abc178_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc178/submissions/16678395

---
+++ atcoder/abc178/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << max({a * c, b * d, a * d, b * c}) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
