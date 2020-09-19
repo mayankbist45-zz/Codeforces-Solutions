3f15d846963cb81e98c4c4c1d76e8c255652aee7
Mayank Bist <mayankbist45@gmail.com>
2020-09-19 17:31:00 +0530

    Add solution for problem `A - Plural Form` Link: https://atcoder.jp/contests/abc179/tasks/abc179_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc179/submissions/16848130

---
+++ atcoder/abc179/A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    if (s.back() == 's')cout << s << "es" << endl;
+    else cout << s << 's' << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
