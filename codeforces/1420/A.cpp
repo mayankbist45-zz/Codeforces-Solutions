f187457456cdc2ba3f9dd1cbfec2a261bc9be14d
Mayank Bist <mayankbist45@gmail.com>
2020-09-24 20:34:00 +0530

    Add solution for problem `A - Cubes Sorting` Link: https://codeforces.com/contest/1420/problem/A Tags: math, sortings, *900 Ref: https://codeforces.com/contest/1420/submission/93675505

---
+++ codeforces/1420/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    bool fd = true;
+    for (int i = 1; i < n; i++) if (v[i] >= v[i - 1])fd = false;
+    if(fd)cout << "NO" << endl;
+    else cout << "YES" << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
