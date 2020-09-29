327b2be8fecaf89da2354275bc430e2847de7795
Mayank Bist <mayankbist45@gmail.com>
2020-09-29 17:03:00 +0530

    Add solution for problem `E - Connected Components?` Link: https://codeforces.com/contest/920/problem/E Tags: data structures, dfs and similar, dsu, graphs, *2100 Ref: https://codeforces.com/contest/920/submission/94203900

--- codeforces/920/E.cpp
+++ codeforces/920/E.cpp
@@ -7,37 +7,65 @@ using namespace std;
 #define maxn 200010
 const int MOD = 1000000007;
 
-set<int> g[maxn], st;
+set<int> g[maxn];
+vector<int> vis;
 int n, m;
 
-int dfs(int u) {
-    int ans = 1;
-    st.erase(u);
-    bool fl = true;
-    while (fl) {
-        fl = false;
-        for (auto x: st) {
-            if (g[u].count(x))continue;
-            ans += dfs(x);
-            fl = true;
-            break;
-        }
+struct disjoint_set {
+    vector<int> par, sz;
+    disjoint_set(int n) : par(n + 1), sz(n + 1, 1) {
+        iota(par.begin(), par.end(), 0ll);
     }
-    return ans;
+    int find(int x) { return par[x] = (x == par[x] ? x : find(par[x])); }
+    void unite(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v)return;
+        if (sz[u] < sz[v]) swap(u, v);
+        par[v] = u;
+        sz[u] += sz[v];
+    }
+};
+
+void dfs(int u, disjoint_set &dsu) {
+    vis[u] = true;
+    int asn = 1;
+    for (int i = 1; i <= n; i++)
+        if (!vis[i] and g[u].find(i) == g[u].end()) {
+            dsu.unite(i, u);
+            dfs(i, dsu);
+        } else if (vis[i] and g[u].find(i) == g[u].end())
+            dsu.unite(i, u);
 }
 
 void solve() {
     cin >> n >> m;
+    vector<int> deg(n + 1, n - 1);
     for (int i = 0, a, b; i < m; i++) {
         cin >> a >> b;
+        deg[a]--, deg[b]--;
         g[a].insert(b);
         g[b].insert(a);
     }
-    for (int i = 1; i <= n; i++)st.insert(i);
-    vector<int> ans;
+    disjoint_set dsu(n);
+    vis = vector<int>(n + 1);
+    int ct = 0, pre = -1;
     for (int i = 1; i <= n; i++)
-        if (st.count(i))
-            ans.push_back(dfs(i));
+        if (deg[i] >= n / 2) {
+            if (pre == -1)pre = i;
+            else dsu.unite(i, pre);
+            vis[i] = 1;
+        }
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i])dfs(i, dsu);
+    }
+    vector<int> ans;
+    vis = vector<int>(n + 1, false);
+    for(int i = 1; i <= n; i++){
+        if(!vis[dsu.find(i)]){
+            ans.push_back(dsu.sz[dsu.find(i)]);
+            vis[dsu.find(i)] = 1;
+        }
+    }
     sort(ans.begin(), ans.end());
     cout << ans.size() << endl;
     for (auto x: ans)cout << x << " ";
