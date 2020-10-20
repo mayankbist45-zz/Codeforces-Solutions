a0c64c06473dadd8c68cf99a59b74d93ade20803
Mayank Bist <mayankbist45@gmail.com>
2020-10-20 20:07:00 +0530

    Add solution for problem `A - Boring Apartments` Link: https://codeforces.com/contest/1433/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1433/submission/96082690

---
+++ codeforces/1433/A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    int x;
+    cin >> x;
+    int n = to_string(x).size();
+    cout << ((x % 10) - 1) * 10 + n * (n + 1)/2 << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
