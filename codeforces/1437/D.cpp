835b2e3b968a7c2db5184463543c0d18a9a1ee10
Mayank Bist <mayankbist45@gmail.com>
2020-10-27 21:42:00 +0530

    Add solution for problem `D - Minimal Height Tree` Link: https://codeforces.com/contest/1437/problem/D Tags: graphs, greedy, shortest paths, trees, *1600 Ref: https://codeforces.com/contest/1437/submission/96915649

--- codeforces/1437/D.cpp
+++ codeforces/1437/D.cpp
@@ -1,6 +1,3 @@
-//20
-//1 17 20 18 12 8 13 16 7 19 3 6 15 10 14 9 5 4 11 2
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -14,23 +11,25 @@ void solve() {
     int n;
     cin >> n;
 
-    vector<int> v(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i];
+    deque<int> dq;
+    for (int i = 0, tp; i < n; ++i) {
+        cin >> tp;
+        dq.push_back(tp);
     }
     int par = 1, ct = 0;
-    for (int i = 1; i < n; i++) {
-        int now = i, coutn = 0;
-        for (int k = 0; k < par and now < n; k++) {
-            int j = now;
-            while (j + 1 < n and v[j + 1] > v[j])j++;
-            int cur = j - now + 1;
-            coutn += cur;
-            now = j + 1;
+    dq.pop_front();
+    while (!dq.empty()) {
+        int layer = 0;
+        for (int i = 0; i < par; i++) {
+            int prev = -1;
+            while (!dq.empty() and (dq.front() > prev or prev == -1)) {
+                prev = dq.front();
+                dq.pop_front();
+                layer++;
+            }
         }
-        par = coutn;
         ct++;
-        i = now - 1;
+        par = layer;
     }
     cout << ct << endl;
 }
