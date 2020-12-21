e7a5c8f96c1463eacd8705cda7a408e3b0524808
Mayank Bist <mayankbist45@gmail.com>
2020-12-21 12:47:00 +0530

    Add solution for problem `A - Three-letter acronym` Link: https://atcoder.jp/contests/abc059/tasks/abc059_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc059/submissions/18920216

---
+++ atcoder/abc059/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    string a, b, c;
+    cin >> a >> b >> c;
+    cout << (char)toupper(a[0]) <<(char) toupper(b[0]) <<(char) toupper(c[0]);
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
