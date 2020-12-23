caecff6c9e94f2c4897e6b1fd0e53231a663d484
Mayank Bist <mayankbist45@gmail.com>
2020-12-23 11:36:00 +0530

    Add solution for problem `A - CODEFESTIVAL 2016` Link: https://atcoder.jp/contests/code-festival-2016-quala/tasks/codefestival_2016_qualA_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/code-festival-2016-quala/submissions/18948457

---
+++ atcoder/code-festival-2016-quala/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 100010
+const int MOD = 1000000007;
+
+void solve() {
+    string s;
+    cin >> s;
+    cout << s.substr(0, 4) << " " << s.substr(4, 8) << endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
