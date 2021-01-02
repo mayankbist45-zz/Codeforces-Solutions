3e73f47060321f70005ae1614c73e063728e073d
Mayank Bist <mayankbist45@gmail.com>
2021-01-02 11:01:00 +0530

    Add solution for problem `E - Number of Simple Paths` Link: https://codeforces.com/contest/1454/problem/E Tags: combinatorics, dfs and similar, graphs, trees, *2000 Ref: https://codeforces.com/contest/1454/submission/103003747

--- codeforces/1454/E.cpp
+++ codeforces/1454/E.cpp
@@ -7,43 +7,67 @@ using namespace std;
 #define maxn 200010
 const int MOD = 1000000007;
 
-set<int> g[maxn];
+vector<int> cycle;
+int vis[maxn], par[maxn];
+vector<int> g[maxn];
+
+bool dfs(int u, int p = -1) {
+    vis[u] = 1;
+    par[u] = p;
+    for (auto x : g[u]) {
+        if (!vis[x]) {
+            bool val = dfs(x, u);
+            if (val)return val;
+        } else if (vis[x] == 1 and p != x) {
+            //cycle found
+            int cur = u;
+            while (cur != x) {
+                cycle.push_back(cur);
+                cur = par[cur];
+            }
+            cycle.push_back(x);
+            return true;
+        }
+    }
+    vis[u] = 2;
+    return false;
+}
+
+int solve(int u) {
+    vis[u] = true;
+    int ret = 1;
+    for (auto x : g[u]) {
+        if (!vis[x] and !binary_search(cycle.begin(), cycle.end(), x)) {
+            ret += solve(x);
+        }
+    }
+    return ret;
+}
 
 void solve() {
     int n;
     cin >> n;
 
-    vector<int> cnt(n + 1, 1);
+    cycle.clear();
     for (int i = 0; i <= n; i++) {
         g[i].clear();
+        vis[i] = false;
     }
 
     for (int i = 0; i < n; i++) {
         int a, b;
         cin >> a >> b;
-        g[a].insert(b);
-        g[b].insert(a);
+        g[a].push_back(b);
+        g[b].push_back(a);
     }
-    queue<int> q;
-    for (int i = 1; i <= n; i++) {
-        if (g[i].size() == 1)
-            q.push(i);
-    }
+    dfs(1);
+    sort(cycle.begin(), cycle.end());
     int ans = 0;
-    while (!q.empty()) {
-        int top = q.front();
-        q.pop();
 
-        cnt[*g[top].begin()] += cnt[top];
-        g[*g[top].begin()].erase(top);
-        cnt[top] = 0;
+    for (int i = 0; i <= n; i++) vis[i] = false;
 
-        if (g[*g[top].begin()].size() == 1)
-            q.push(*g[top].begin());
-    }
-
-    for(int i = 1; i <= n; i++){
-        int val = cnt[i];
+    for (auto x: cycle) {
+        int val = solve(x);
         ans += val * (val - 1);
         ans += 2 * (n - val) * val;
     }
