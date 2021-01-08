7320821d03d3d32b057afeddf647e99678dc1323
Mayank Bist <mayankbist45@gmail.com>
2021-01-08 16:21:00 +0530

    Add solution for problem `C - Low Elements` Link: https://atcoder.jp/contests/abc152/tasks/abc152_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc152/submissions/19263901

---
+++ atcoder/abc152/C.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define maxn 200010
+const int MOD = 1000000007;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    int mn = LLONG_MAX;
+    int ct = 0;
+    for (auto x: v) {
+        ct += mn > x;
+        mn = min(mn, x);
+    }
+    cout << ct;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+//    cin >> t;
+
+    while (t--)
+        solve();
+    return 0;
+}
